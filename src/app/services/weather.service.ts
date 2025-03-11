5a60f13fd804019204e1c647d7708ee8ca88596f
Atriox <developerjarm116@gmail.com>
2025-03-10 23:02:02 -0500

    Developer APP weather 2

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
- 
-private apikey = '06ce7fd6ecc74a30ab023002252302' 
-private apiurl = 'http://api.weatherapi.com/v1/current.json'
+  private apiKey = '06ce7fd6ecc74a30ab023002252302'; 
+  private apiUrl = 'http://api.weatherapi.com/v1/current.json';
 
-  constructor( private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getWeather(city:string, country:string):Observable<any>{
-    let countrym = city + ',' + country
-    return this.http.get(`${this.apiurl}?key=${this.apikey}&q=${countrym}&aqi=no&lang=es`)
+  getWeather(city: string, country: string): Observable<any> {
+    const location = encodeURIComponent(`${city},${country}`);
+    const url = `${this.apiUrl}?key=${this.apiKey}&q=${location}&aqi=no&lang=es`;
+
+    const headers = new HttpHeaders({
+      'Accept': 'application/json'
+    });
+
+    return this.http.get(url, { headers }).pipe(
+      catchError((error) => {
+        console.error('Error al obtener el clima:', error);
+        return throwError(() => new Error('No se pudo obtener la información del clima'));
+      })
+    );
   }
-
-
 }
